558f982403a5f4421aecc88a2fd44fd1376dbb4e
jfsenechal <jfsenechal@gmail.com>
2025-03-18 20:12:12 +0100

    add user

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Constant\NavigationGroupEnum;
+use App\Filament\Actions\GeneratePasswordAction;
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\Role;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    public static function getNavigationGroup(): ?string
+    {
+        return NavigationGroupEnum::SETTINGS->getLabel();
+    }
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('first_name')
+                    ->required()
+                    ->maxLength(120),
+                Forms\Components\TextInput::make('second_name')
+                    ->required()
+                    ->maxLength(120),
+                Forms\Components\TextInput::make('email')
+                    ->required()
+                    ->email()
+                    ->unique(ignoreRecord: true)
+                    ->label('Email address')
+                    ->maxLength(120),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required(fn($livewire) => $livewire instanceof Pages\CreateUser)
+                    ->revealable(filament()->arePasswordsRevealable())
+                    ->rule(Password::default())
+                    ->autocomplete('new-password')
+                    ->suffixActions([
+                        GeneratePasswordAction::make(),
+                    ])
+                    ->maxLength(255),
+                Forms\Components\Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->multiple()
+                    ->preload(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('first_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('second_name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('roles.name'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('roles')
+                    ->relationship('roles', 'name'),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+
+    public static function canAccess(): bool
+    {
+        return Auth::getUser()->hasRole(Role::ROLE_ADMIN);
+    }
+}
