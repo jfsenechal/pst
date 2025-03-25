8faaae75e1b97184eb5792b979d7de70114f0930
jfsenechal <jfsenechal@gmail.com>
2025-03-25 14:44:39 +0100

    add actions

--- app/Filament/Resources/PartnerResource/Pages/ViewPartner.php
+++ app/Filament/Resources/PartnerResource/Pages/ViewPartner.php
@@ -2,8 +2,13 @@
 
 namespace App\Filament\Resources\PartnerResource\Pages;
 
+use App\Filament\Resources\ActionResource;
 use App\Filament\Resources\PartnerResource;
+use App\Models\Action;
 use Filament\Actions;
+use Filament\Infolists\Components\Fieldset;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewPartner extends ViewRecord
@@ -18,5 +23,40 @@ class ViewPartner extends ViewRecord
             Actions\DeleteAction::make()
                 ->icon('tabler-trash'),
         ];
+    }
+
+    public function getTitle(): string
+    {
+        return $this->record->name.' '.$this->record->initials ?? 'Empty name';
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                TextEntry::make('email')
+                    ->icon('tabler-mail'),
+                TextEntry::make('phone')
+                    ->icon('tabler-phone'),
+                TextEntry::make('description')
+                    ->label(false)
+                    ->html()
+                    ->columnSpanFull()
+                    ->prose(),
+                Fieldset::make('actions')
+                    ->relationship('actions')
+                    ->label('Actions')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Nom')
+                            ->columnSpanFull()
+                            ->url(
+                                fn(Action $action) => ActionResource::getUrl(
+                                    'view',
+                                    parameters: ['record' => $action->id]
+                                )
+                            ),
+                    ]),
+            ]);
     }
 }
